a08e353752a0aadaf932d8f50bec64acd156a07b
tonymichaelhead <tonymichaelhead@gmail.com>
2017-07-29 22:50:07 -0700

    Setup db config and model

--- db/config.js
+++ db/config.js
@@ -1,7 +1,16 @@
 //TODO: set up your database configuration and connection here
 
-const Sequelize = require('sequelize')
+const mongoose = require('mongoose');
 // ** remember to hide your db config information!
 const config = require('../config') 
 
 // **other files will need access to information in here**
+mongoose.connect('mongodb://localhost/movies');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    'yeaaaaah buddy DB is connect'
+});
+ 
--- db/models/movieModel.js
+++ db/models/movieModel.js
@@ -1,11 +1,15 @@
-//TODO: define your db schemas here
-
-const Sequelize = require('sequelize')
 const db = require('../config')
 
-const Movie = db.define('movies', {
-  //TODO: FILL ME IN
-})
+const movieSchema = mongoose.Schema({
+    title: String,
+    copies: Number,
+    rating: Number,
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
+
 
 
 // **other files will need access to information in here**
