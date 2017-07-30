6e3c986d8f0edc703bc9c96171ea8801c7faecd1
tonymichaelhead <tonymichaelhead@gmail.com>
2017-07-30 00:50:58 -0700

    Server handling post request and inserting data into db

--- server/controllers/movie.controller.js
+++ server/controllers/movie.controller.js
@@ -4,16 +4,26 @@ Movie = require('../../db/models/movieModel.js')
 module.exports = {
   getAllMovies: (req, res) => {
     Movie.find()
-      .then((result) => {
+      .then(result => {
         res.status(200).send(result);
       })
       .catch(err => {
         res.send(err);
       })
   },
-  // addMovie: (req, res) => {
-  //   // TODO: FILL ME IN
-    
-  // }
+  addMovie: (req, res) => {
+    let newMovie = new Movie({
+        title: req.body.title,
+        copies: req.body.copies,
+        rating: req.body.rating,
+    })
+    newMovie.save((err, newMovie) => {
+      if(err) return console.error(err);
+      console.log(`saved ${newMovie.title}`);
+    })
+    .then(result => {
+      res.status(201).send(result);
+    })
+  }
 }
 
--- server/router/blockbuster.router.js
+++ server/router/blockbuster.router.js
@@ -1,12 +1,15 @@
 const router = require('express').Router()
 const controller = require('../controllers/movie.controller.js')
 router.route('/movies')
-  .get((req,res) => {
+  .get((req, res) => {
     console.log('GET GOT');
     controller.getAllMovies(req, res);
-  });
+  })
   
-  // .post(/*TODO: ADD YOUR CONTROLLER*/)
+  .post((req, res) => {
+    console.log('POST GOT');
+    controller.addMovie(req, res);
+  })
 
 // **other files will need access to information in here**
 module.exports = router;
