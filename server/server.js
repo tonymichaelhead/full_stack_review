d21b4b2e063d1ae29641a7d113508070faac39b9
tonymichaelhead <tonymichaelhead@gmail.com>
2017-07-29 19:27:51 -0700

    Server up and running

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
+const express = require('express');
+const path = require('path');
+const parser = require('body-parser');
+const router = require('./router/blockbuster.router');
+
 //TODO: fill this page in!
 
 // require your dependencies
 
 // create an instance of your express server
-
+const app = express();
 
 // depending on which front end framework you're using, 
 // serve your static files
-
+//app.use()
 // add middleware
-
+app.use(parser.json());
+app.use(parser.urlencoded( { extended: true }));
+app.use(express.static(path.join(__dirname, '../static')));
 // define your routes
 
+//After fleshing out exporting router, hook this up!!
+// app.use('/api', router);
 // set up your server to listen on your port of choice
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('Server is listening on port ', port);
+});
